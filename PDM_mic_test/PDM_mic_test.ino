3b7b39cdb891483e95fce0b305f5e03f9f5a5181
sadiet20 <sadierosethomas@gmail.com>
2022-03-29 19:41:34 -0700

    initial commit

---
+++ PDM_mic_test/PDM_mic_test.ino
@@ -0,0 +1,91 @@
+/*
+  This example reads audio data from the on-board PDM microphones, and prints
+  out the samples to the Serial console. The Serial Plotter built into the
+  Arduino IDE can be used to plot the audio data (Tools -> Serial Plotter)
+
+  Circuit:
+  - Arduino Nano 33 BLE Sense board
+
+  This example code is in the public domain.
+*/
+
+#include <PDM.h>
+
+// buffer to read samples into, each sample is 16-bits
+short sampleBuffer[256];
+
+// number of samples read
+volatile int samplesRead;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // configure the data receive callback
+  PDM.onReceive(onPDMdata);
+
+  // optionally set the gain, defaults to 20
+  // PDM.setGain(30);
+
+  PDM.setBufferSize(256);
+
+  // initialize PDM with:
+  // - one channel (mono mode)
+  // - a 16 kHz sample rate
+  if (!PDM.begin(1, 16000)) {
+    Serial.println("Failed to start PDM!");
+    while (1);
+  }
+}
+
+void loop() {
+  // wait for samples to be read
+  if (samplesRead) {
+    //Serial.println("NEW DATA");
+    //delay(500);
+    //Serial.print("Samples read: ");
+    //Serial.println(samplesRead);
+    // print samples to the serial monitor or plotter
+    for (int i = 0; i < samplesRead; i++) {
+      Serial.println(sampleBuffer[i]);
+      // check if the sound value is higher than 500
+      if (sampleBuffer[i]>=500){
+        digitalWrite(LEDR,LOW);
+        digitalWrite(LEDG,HIGH);
+        digitalWrite(LEDB,HIGH);
+        //Serial.println(">= 500");
+        //delay(1000);
+      }
+      // check if the sound value is higher than 250 and lower than 500
+      if (sampleBuffer[i]>=250 && sampleBuffer[i] < 500){
+        digitalWrite(LEDB,LOW);
+        digitalWrite(LEDR,HIGH);
+        digitalWrite(LEDG,HIGH);
+        //Serial.println("between 250 and 500");
+        //delay(1000);
+      }
+      //check if the sound value is higher than 0 and lower than 250
+      if (sampleBuffer[i]>=0 && sampleBuffer[i] < 250){
+        digitalWrite(LEDG,LOW);
+        digitalWrite(LEDR,HIGH);
+        digitalWrite(LEDB,HIGH);
+        //Serial.println("< 250");
+        //delay(1000);
+      }
+    }
+
+    // clear the read count
+    samplesRead = 0;
+  }
+}
+
+void onPDMdata() {
+  // query the number of bytes available
+  int bytesAvailable = PDM.available();
+
+  // read into the sample buffer
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16-bit, 2 bytes per sample
+  samplesRead = bytesAvailable / 2;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Squeak Factor
+## This code is part of a project to rate shoes based on their "Squeak Factor" which is a numerical rating of how squeaky the shoe is on various surfaces.
